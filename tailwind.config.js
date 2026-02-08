eb39283cfc616135c9785338213f814807898499
Mustafa <m.a.biltekin@gmail.com>
2026-02-08 21:21:28 +0300

    Fix tailwind theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
 module.exports = {
-  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
+  content: [
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}"
+  ],
+
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#0f3d2e",
+          light: "#145c43",
+        },
+        gold: {
+          DEFAULT: "#c7a14a",
+          light: "#e7c873",
+        }
+      }
+    }
+  },
+
+  plugins: []
 };
