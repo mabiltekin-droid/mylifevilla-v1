26b0d5b73dd6985f02b93d17cfcaa24fdcb5d538
Mustafa <m.a.biltekin@gmail.com>
2026-02-09 18:34:16 +0300

    Fix Decap CMS OAuth for Vercel

--- pages/api/auth/callback.js
+++ pages/api/auth/callback.js
@@ -1,15 +1,7 @@
 import { createOAuthAppAuth } from "@octokit/auth-oauth-app";
-import { Octokit } from "@octokit/rest";
 
 export default async function handler(req, res) {
   const { code } = req.query;
-
-  if (!code) {
-    // GitHub login'e yönlendir
-    const redirect = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`;
-    res.redirect(302, redirect);
-    return;
-  }
 
   try {
     const auth = createOAuthAppAuth({
@@ -17,7 +9,10 @@ export default async function handler(req, res) {
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     });
 
-    const { token } = await auth({ type: "oauth-user", code });
+    const { token } = await auth({
+      type: "oauth-user",
+      code,
+    });
 
     res.setHeader("Content-Type", "application/json");
     res.status(200).send(
---
+++ pages/api/auth/login.js
@@ -0,0 +1,4 @@
+export default function handler(req, res) {
+  const redirect = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=repo`;
+  res.redirect(302, redirect);
+}
